62be2c525ad54ddab5d81ac0e8b025f4e26102ef
yg <1454109713@qq.com>
2019-03-09 14:07:48 +0800

    2019.3.9第二次提交

--- spring-security-demo/src/main/java/com/imooc/web/controller/UserController.java
+++ spring-security-demo/src/main/java/com/imooc/web/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.imooc.web.controller;
 
 import com.imooc.dto.User;
+import com.imooc.dto.UserQueryCondition;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,8 +18,28 @@ import java.util.List;
  */
 @RestController
 public class UserController {
+//    @RequestMapping(value="/user",method= RequestMethod.GET)
+//    public List<User> Query(@RequestParam(name = "username",required = false,defaultValue = "tom") String nickname){
+//        System.out.println(nickname);
+//        List<User> users=new ArrayList<>();
+//        users.add(new User());
+//        users.add(new User());
+//        users.add(new User());
+//        return users;
+//    }
+
+
     @RequestMapping(value="/user",method= RequestMethod.GET)
-    public List<User> Query(){
-        return null;
+    public List<User> Query(UserQueryCondition condition, @PageableDefault(page = 2,size = 17,sort = "username,asc") Pageable pageable){
+        System.out.println(ReflectionToStringBuilder.toString(condition, ToStringStyle.MULTI_LINE_STYLE));
+        System.out.println(pageable.getPageSize());
+        System.out.println(pageable.getPageNumber());
+        System.out.println(pageable.getSort());
+
+        List<User> users=new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        return users;
     }
 }
--- spring-security-demo/src/main/java/com/imooc/web/controller/UserControllerTest.java
+++ spring-security-demo/src/main/java/com/imooc/web/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.imooc.web.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,12 +13,15 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.concurrent.Callable;
+
 /**
  *
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserControllerTest {
+    @Autowired
     private WebApplicationContext wac;
     private MockMvc mockMvc;
 
@@ -26,16 +30,20 @@ public class UserControllerTest {
         mockMvc= MockMvcBuilders.webAppContextSetup(wac).build();
     }
     @Test
-    public void whenQuerySuccess(){
-        try {
-            mockMvc.perform(MockMvcRequestBuilders.get("/user")
-                    .contentType(MediaType.APPLICATION_JSON_UTF8))
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers
-                            .jsonPath("$.length()")
-                            .value(3));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void whenQuerySuccess() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/user")
+                .param("username","jojo")
+                .param("age","18")
+                .param("ageTo","60")
+                .param("xxx","yyy")
+                /*  .param("size","15")
+                  .param("page","3")
+                  .param("sort","age,desc")*/
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers
+                        .jsonPath("$.length()")
+                        .value(3));
     }
 }
+
